e6797b67a478638a32e553d16f7e8b7ef4c0006c
Luca Osti <luca.d.osti@gmail.com>
2021-11-29 15:45:20 +0100

    Use go-yaml

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 
 	"github.com/ardanlabs/conf/v2"
-	confYaml "github.com/ardanlabs/conf/v2/yaml"
+	"github.com/goccy/go-yaml"
 	"gitlab.com/top-solution/go-libs/frequency"
 	"gitlab.com/top-solution/go-libs/version"
 )
@@ -16,14 +16,36 @@ import (
 var baseFlags = struct {
 	// The config file path: it's only read from flags or env vars
 	ConfigFile string `yaml:"-" conf:"default:conf.yml"`
-	// The build info of the app
+	// The build info of the app It's used by ardanlabs/conf
 	Build string `yaml:"-"`
-	// The
+	// The description of the app version. It's used by ardanlabs/conf
 	Desc string `yaml:"-"`
 }{}
 
+type yamlConfParser struct {
+	data []byte
+}
+
+func (y yamlConfParser) Process(prefix string, cfg interface{}) error {
+	err := yaml.Unmarshal(y.data, cfg)
+	if err != nil {
+		return fmt.Errorf("unmarshal yaml: %w", err)
+	}
+	return nil
+}
+
+func readYamlConf(path string) (c yamlConfParser, err error) {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return c, fmt.Errorf("read config yaml: %w", err)
+	}
+	return yamlConfParser{
+		data: content,
+	}, nil
+}
+
 // ParseConfig parses a file config, fetching the (optional) config file name from a flag or env var
-func ParseConfig(cfg interface{}) error {
+func ParseConfigAndVersion(cfg interface{}) error {
 	// Build version info
 	versionInfo := version.GetInfo()
 	baseFlags.Desc = fmt.Sprintf("%s - %s", versionInfo.Commit, versionInfo.BuildDate)
@@ -37,6 +59,8 @@ func ParseConfig(cfg interface{}) error {
 				help, _ = conf.UsageInfo("", cfg)
 			}
 			fmt.Println(help)
+			fmt.Println("BASE OPTIONS")
+			fmt.Println("  --config-file\t\t <string> set the config file path (default: conf.yml)")
 			os.Exit(0)
 			return nil
 		}
@@ -49,11 +73,11 @@ func ParseConfig(cfg interface{}) error {
 // ParseConfig parses a file config, given the file path, expecting the prefix
 // See ardanlabs/conf's docs to see the usefulness of a prefix
 func ParseConfigWithPrefix(cfg interface{}, path string, prefix string) error {
-	content, err := ioutil.ReadFile(path)
+	yamlConfData, err := readYamlConf(path)
 	if err != nil {
 		return err
 	}
-	help, err := conf.Parse(prefix, cfg, confYaml.WithData(content))
+	help, err := conf.Parse(prefix, cfg, yamlConfData)
 	if err != nil {
 		if errors.Is(err, conf.ErrHelpWanted) {
 			fmt.Println(help)
@@ -77,7 +101,7 @@ type EmailConfig struct {
 
 // LogConfig  is a default config struct  for logs
 type LogConfig struct {
-	Path       string `yaml:"path" conf:"default:logs"`
+	Path       string `yaml:"path" conf:"default:logs help:T"`
 	Expiration struct {
 		Frequency frequency.Frequency `yaml:"frequency"`
 	} `yaml:"expiration"`
